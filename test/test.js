f8901584e6ebc724df252af42db571ddf4a6a7bf
Gertjan De Mulder <gertjan.demulder@ugent.be>
2022-07-13 16:18:56 +0200

    (wip) draft test code

--- test/test.js
+++ test/test.js
@@ -1,42 +1,67 @@
-
 const fs = require('fs');
 const path = require('path');
-// const assert = require('assert');
-// const factory = require('rdf-ext')
-// const ParserN3 = require('@rdfjs/parser-n3')
-// const SHACLValidator = require('rdf-validate-shacl');
-// async function loadDataset (filePath) {
-//     const stream = fs.createReadStream(filePath)
-//     const parser = new ParserN3({ factory })
-//     return factory.dataset().import(parser.import(stream))
-//   }
+const assert = require('assert');
+const SHACLValidator = require("shacl-js");
+
+/**
+ * Validator class for SHACL.
+ */
+class ShaclValidator {
+    /**
+     * Validates SHACL data using shapes.
+     * @param data {string} data in turtle
+     * @param shapes {string} shapes in turtle
+     * @returns {Promise<Object>} Returns a promise which resolves to a SHACL report.
+     */
+    static validate(data, shapes) {
+        return new Promise((resolve, reject) => {
+            new SHACLValidator().validate(
+                data,
+                "text/turtle",
+                shapes,
+                "text/turtle",
+                (e, report) => {
+                    e ? reject(e) : resolve(report);
+                }
+            );
+        });
+    }
+}
 
 describe('SHACL Validation tests', async function () {
 
     const dirTestResources = path.join('test', 'resources');
-    const validFunctionDescriptions = fs.readdirSync(path.join(dirTestResources, 'valid')).filter(x => x.endsWith('.ttl'));
-    const invalidFunctionDescriptions = fs.readdirSync(path.join(dirTestResources, 'invalid')).filter(x => x.endsWith('.ttl'));
+    const dirValidTestResourcs = path.join(dirTestResources, 'valid');
+    const dirInvalidTestResources = path.join(dirTestResources, 'invalid');
+    const validFunctionDescriptions = fs.readdirSync(dirValidTestResourcs).filter(x => x.endsWith('.ttl')).map( x => path.join(dirValidTestResourcs, x) );
+    const invalidFunctionDescriptions = fs.readdirSync(path.join(dirTestResources, 'invalid')).filter(x => x.endsWith('.ttl')).map( x => path.join(dirInvalidTestResources, x) );
 
-    // const shapes = await loadDataset('shape.ttl');
+    // FnO SHACL shape
+    const shapes = fs.readFileSync("./shape.ttl").toString();
 
     describe('Valid descriptions', async function () {
         // Iterate over VALID test resources
         validFunctionDescriptions.forEach( (f) => {
             it(`${f} should be valid`, async function() {
-                // const shapes = await loadDataset('shape.ttl');
-                const stophere = 0;
+
+                const data = fs.readFileSync(f).toString();
+                const report = await ShaclValidator.validate(data, shapes);
+                assert(report.conforms())
             })
         });
 
     });
-    describe('Invalid descriptions', async function () {
 
+    describe('Invalid descriptions', async function () {
          // Iterate over INVALID test resources
         invalidFunctionDescriptions.forEach( (f) => {
-            console.log('f:', f);
+            it(`${f} should not be valid`, async function() {
+
+                const data = fs.readFileSync(f).toString();
+                const report = await ShaclValidator.validate(data, shapes);
+                assert(!report.conforms())
+            })
         });
     });
-
-
 
 });
